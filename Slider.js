a3a570d83d10426a48e94f851f08536481057ae5
JspBack <bekciyusuf336@gmail.com>
2023-10-04 19:39:55 +0300

    second commit

---
+++ Slider.js
@@ -0,0 +1,65 @@
+import "./Slider.css";
+import bg1 from "background";
+import bg2 from "background";
+import bg3 from "background";
+
+const backgrounds = [bg1, bg2, bg3];
+const delay = 2500;
+
+function Slideshow() {
+  const [index, setIndex] = React.useState(0);
+  const timeoutRef = React.useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  React.useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === backgrounds.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
+  return (
+    <div className="slideshow">
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {backgrounds.map((backgroundColor, index) => (
+          <img
+            className="slide"
+            key={index}
+            src={backgroundColor}
+            alt={index}
+          ></img>
+        ))}
+      </div>
+
+      <div className="slideshowDots">
+        {backgrounds.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? " active" : ""}`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Slideshow;
